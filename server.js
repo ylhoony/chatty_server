3cb83e13a0cc076b9da4190961465746273ddcad
ylhoony <yl.hoony@gmail.com>
2017-02-11 22:32:41 +0000

    added user count display

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const WebSocket = require('ws');
 const express = require('express');
 const SocketServer = require('ws').Server;
+const uuid = require('node-uuid');
 
 // Set the port to 4000
 const PORT = 4000;
@@ -15,18 +16,82 @@ const server = express()
 // Create the WebSockets server
 const webSocketServer = new SocketServer({ server });
 
+
+
+// Broadcast to all. - where should I put this?
+webSocketServer.broadcast = function broadcast(data) {
+  // console.log("in broadcast");
+  // console.log("broadcast"+data);
+  webSocketServer.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+
+      client.send(JSON.stringify(data));
+    }
+  });
+};
+
+
+const COLORS = ["red", "orange", "blue", "green", "black"]
+
+function getRandomColor() {
+  return COLORS[Math.floor(Math.random()*COLORS.length)]
+}
+
+
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 webSocketServer.on('connection', (socketConnection) => {
   console.log('Client connected');
+  console.log(webSocketServer.clients.size);
+
+  let userCount = {
+    type: 'userCount',
+    userCount: webSocketServer.clients.size
+  }
+
+  webSocketServer.broadcast(userCount);
+
 
   socketConnection.on('message', function(message) {
     let data = JSON.parse(message);
-    console.log("Message received in 4000: ", data.username, " said ", data.content);
+    // console.log("message parsed: ", data);
+    switch(data.type) {
+      case "postMessage":
+        data['id'] = uuid.v4();
+        data['type'] = 'incomingMessage';
+        console.log(data);
+        // socketConnection.send(JSON.stringify(data));
+        webSocketServer.broadcast(data);
+        break;
+      case "postNotification":
+        // handle incoming notification
+        data['type'] = 'incomingNotification';
+        webSocketServer.broadcast(data);
+        break;
+      default:
+        // show an error in the console if the message type is unknown
+        throw new Error("Unknown event type " + data.type);
+    }
+
 
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  socketConnection.on('close', () => console.log('Client disconnected'));
+  socketConnection.on('close', () => {
+    console.log('Client disconnected');
+
+    let userCount = {
+      type: 'userCount',
+      userCount: webSocketServer.clients.size
+    }
+    webSocketServer.broadcast(userCount);
+  });
 });
+
+
+
+
+
+
